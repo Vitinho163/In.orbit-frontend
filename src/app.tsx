d72119ea574d9502877c053e9a666a0bb7ca41b4
João Victor <victor.jvsb18@gmail.com>
2024-09-10 19:38:01 -0300

    Feat: create first page and components

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,22 @@
+import { Plus } from 'lucide-react'
+
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustation.svg'
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <h1 className="bg-black text-white text-3xl">hello World!</h1>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="in.orbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <Button>
+        <Plus className='size-4' />
+        Cadastrar meta
+      </Button>
+    </div>
+  )
 }
